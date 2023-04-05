f34a20e501c93ea394b2439b7d73912251eee6fd
CarlosViniciusBizinottoPrates <viniciusbizinotto@gmail.com>
2023-04-04 22:58:19 -0300

    Implementation DB insert, update, delete and getAll

--- app/src/main/java/com/example/appguests/repository/GuestRepository.kt
+++ app/src/main/java/com/example/appguests/repository/GuestRepository.kt
@@ -8,7 +8,7 @@ import java.lang.Exception
 
 //Essa camada quem lida com os dados do banco
 
-class GuestRepository private constructor(context: Context){
+class GuestRepository private constructor(context: Context) {
 
     //Ligação entre GuestRepository e GuestDataBase -> Quando eu instanciar a GuestRepository a GuestDataBase ja vai ser instanciada
     //Como GuestDataBase tem um context, deve ser passado o contexto aqui tambem no constructor
@@ -17,11 +17,11 @@ class GuestRepository private constructor(context: Context){
 
 
     //Singleton
-    companion object{
+    companion object {
         private lateinit var repository: GuestRepository
 
         fun getInstance(context: Context): GuestRepository {
-            if(Companion::repository.isLateinit){
+            if (Companion::repository.isLateinit) {
                 repository = GuestRepository(context)
 
             }
@@ -38,18 +38,91 @@ class GuestRepository private constructor(context: Context){
             val presence = if (guest.presence) 1 else 0
 
             val values = ContentValues()
-            values.put(DataBaseConstants.GUEST.COLUMNS.NAME,guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
             values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
 
             db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
 
-             true
+            true
         } catch (e: Exception) {
-             false
+            false
         }
     }
 
-    fun update(){
+    fun update(guest: GuestModel): Boolean {
+        return try {
+
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + "id = ?"
+            val args = arrayOf(guest.id.toString())
+
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+    }
+
+    fun delete(id: Int): Boolean {
+        return try {
+            //Abertura de conexão com o banco
+            val db = guestDataBase.writableDatabase
+
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + "id = ?"
+            val args = arrayOf(id.toString())
+
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+    }
+
+    fun getALL(): List<GuestModel> {
+
+        val list = mutableListOf<GuestModel>()
+        try {
+
+            val db = guestDataBase.readableDatabase
+
+            val selection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val cursor =  db.query(DataBaseConstants.GUEST.TABLE_NAME, selection, null, null, null, null, null)
+
+            if (cursor != null && cursor.count > 0){
+                while (cursor.moveToNext()){
+
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                    val presence = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE))
+
+                    list.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            cursor.close()
+        } catch (e: Exception){
+          return list
+        }
+        return list
+
 
     }
 
